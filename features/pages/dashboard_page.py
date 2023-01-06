97940c89b84cbc15c6bf525719786ebde9c9e7bc
Reanold Piatrenka <ronny812@gmail.com>
2023-01-06 10:35:20 +0200

    Fix tests

--- features/pages/dashboard_page.py
+++ features/pages/dashboard_page.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class DashboardPageSelectors:
 
-    DASHBOARD_PAGE_TAB = (By.XPATH, ".//a[@id='menu_dashboard_index']")
+    DASHBOARD_PAGE_TAB = (By.XPATH, ".//p[@class='oxd-userdropdown-name']")
--- features/pages/login_page.py
+++ features/pages/login_page.py
@@ -5,6 +5,6 @@ class LoginPageSelectors:
 
     APPLICATION_URL = "https://opensource-demo.orangehrmlive.com/index.php"
     LOGIN_PAGE_TITLE = "OrangeHRM"
-    LOGIN_FIELD = (By.XPATH, ".//input[@id='txtUsername']")
-    PASSWORD_FIELD = (By.XPATH, ".//input[@id='txtPassword']")
-    LOGIN_BUTTON = (By.XPATH, ".//input[@id='btnLogin']")
+    LOGIN_FIELD = (By.XPATH, ".//input[@name='username']")
+    PASSWORD_FIELD = (By.XPATH, ".//input[@name='password']")
+    LOGIN_BUTTON = (By.XPATH, ".//button[@type='submit']")
